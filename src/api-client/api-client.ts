024bab9f59eca2d26b26fd1635710d339cc1d47e
hellwind2019 <daniltorianik2016@gmail.com>
2025-09-15 16:11:20 +0300

    Change url to env

--- src/api-client/api-client.ts
+++ src/api-client/api-client.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "https://superhero-database-rqr7.onrender.com", // your backend URL
-  // baseURL: "http://127.0.0.1:4000",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export default apiClient;
